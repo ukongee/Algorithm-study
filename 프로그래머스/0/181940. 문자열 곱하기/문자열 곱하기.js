131374a8996dd6598367a8e109f56a145915dfe8
박유경 <165669386+ukongee@users.noreply.github.com>
2025-03-19 19:25:20 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,7 +1,2 @@
-function solution(my_string, k) {
-    let answer = '';
-    for(let i=0; i<k; i++){
-        answer += my_string;
-    }
-    return answer;
-}
+const solution = (my_string, k) => my_string.repeat(k);
+
